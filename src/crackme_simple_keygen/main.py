179e7d272c505345ff1d73360bb2b357a810a1ed
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-06-30 01:41:30 +0200

    :sparkles: Add serial keygen

--- src/crackme_simple_keygen/main.py
+++ src/crackme_simple_keygen/main.py
@@ -1,12 +1,28 @@
+from random import randint
+
 import typer
 
 app = typer.Typer()
 
+ASCII_MIN = 32  # ' '
+ASCII_MAX = 127  # ~
+PASSWORD_LEN = 16
+
 
 @app.command()
-def keygen() -> None:
-    typer.echo("Implement your keygen for crackme_simple_keygen here.")
+def keygen():
+    typer.echo(generate_serial())  # pragma: no cover
+
+
+def generate_serial() -> str:
+    start_index = _random()
+    serial = [chr(i) for i in range(start_index, start_index + PASSWORD_LEN)]
+    return "".join(serial)
+
+
+def _random() -> int:
+    return randint(ASCII_MIN, ASCII_MAX - PASSWORD_LEN)
 
 
 if __name__ == "__main__":
-    app()
+    app()  # pragma: no cover
